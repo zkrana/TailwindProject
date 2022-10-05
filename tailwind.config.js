a2bf8d1dff734ca75de7fe586076072a28c0335f
Zk Rana <zkranao@gmail.com>
2022-10-05 19:33:36 +0600

    New Issues fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,55 +1,25 @@
 module.exports = {
   content: ['./*.html'],
+  screens: {
+    sm: "640px",
+    // => @media (min-width: 640px) { ... }
+    md: "768px",
+    // => @media (min-width: 768px) { ... }
+    lg: "1024px",
+    // => @media (min-width: 1024px) { ... }
+    xl: "1280px",
+    // => @media (min-width: 1280px) { ... }
+  },
   theme: {
     extend: {
-      colors: {
-        brightRed: 'hsl(12, 88%, 59%)',
-        brightRedLight: 'hsl(12, 88%, 69%)',
-        brightRedSupLight: 'hsl(12, 88%, 95%)',
-        darkBlue: 'hsl(228, 39%, 23%)',
-        darkGrayishBlue: 'hsl(227, 12%, 61%)',
-        veryDarkBlue: 'hsl(233, 12%, 13%)',
-        veryPaleRed: 'hsl(13, 100%, 96%)',
-        veryLightGray: 'hsl(0, 0%, 98%)',
-      },
     },
     container: {
       center: true,
-      screens: {
-        sm: "640px",
-        // => @media (min-width: 640px) { ... }
-        md: "768px",
-        // => @media (min-width: 768px) { ... }
-        lg: "1024px",
-        // => @media (min-width: 1024px) { ... }
-        xl: "1280px",
-        // => @media (min-width: 1280px) { ... }
-      },
     },
     fontFamily: {
       sans: ['Graphik', 'sans-serif'],
       serif: ['Merriweather', 'serif'],
     },
   },
-  plugins: [
-    // function ({ addComponents }) {
-    //   addComponents({
-    //     '.container': {
-    //       maxWidth: '100%',
-    //       '@screen sm': {
-    //         maxWidth: '640px',
-    //       },
-    //       '@screen md': {
-    //         maxWidth: '768px',
-    //       },
-    //       '@screen lg': {
-    //         maxWidth: '1024px',
-    //       },
-    //       '@screen xl': {
-    //         maxWidth: '1240px',
-    //       },
-    //     }
-    //   })
-    // }
-  ],
+  plugins: [ ],
 }
